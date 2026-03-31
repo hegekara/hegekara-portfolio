eefde2a0d3116a8e1e7d2e6b0206cdf1ba4d757d
Hilmi Ege Kara <hegekara48@gmail.com>
2026-03-31 14:01:08 +0300

    edited for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     proxy: {
       // "/github-api" ile başlayan istekleri yakala ve github.com'a yönlendir
